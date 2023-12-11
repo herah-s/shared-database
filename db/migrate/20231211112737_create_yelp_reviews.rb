3a2a0c327e59a0e73b608f669e5755895e19a197
Rina Iwamine <leddihome@gmail.com>
2023-12-11 20:34:19 +0900

    fixed capitalization

--- db/migrate/20231211112737_create_yelp_reviews.rb
+++ db/migrate/20231211112737_create_yelp_reviews.rb
@@ -1,7 +1,7 @@
 class CreateYelpReviews < ActiveRecord::Migration[7.1]
   def change
     create_table :yelp_reviews do |t|
-      t.references :YelpRestaurant, null: false, foreign_key: true
+      t.references :yelp_restaurant, null: false, foreign_key: true
       t.text :content
       t.integer :rating
 
