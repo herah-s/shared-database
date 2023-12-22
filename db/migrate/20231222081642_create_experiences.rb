c180d4316ab4f2c1eb2bb8f6b636337a4ba1cf50
Rina Iwamine <leddihome@gmail.com>
2023-12-22 17:27:07 +0900

    added plain name tables for kt

---
+++ db/migrate/20231222081642_create_experiences.rb
@@ -0,0 +1,14 @@
+class CreateExperiences < ActiveRecord::Migration[7.1]
+  def change
+    create_table :experiences do |t|
+      t.string :name
+      t.text :description
+      t.float :price
+      t.string :category
+      t.string :address
+      t.references :host, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
